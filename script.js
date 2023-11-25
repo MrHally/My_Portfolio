05054139e1c8ae7c66e06dae0ad22a9b38abfa39
MrHally <nhatanh.personal@gmail.com>
2023-11-25 20:23:47 +0700

    inital commit

---
+++ script.js
@@ -0,0 +1,11 @@
+import AOS from 'aos';
+import 'vendor/aos/aos.css';
+
+AOS.init();
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
